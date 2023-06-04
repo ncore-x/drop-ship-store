965d05edc80ad82db81d9fd1bebc795e74037a79
ncore-x <135090444+ncore-x@users.noreply.github.com>
2023-06-04 23:35:50 +1100

    second commit

--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
     path('admin/', admin.site.urls),
 ]
