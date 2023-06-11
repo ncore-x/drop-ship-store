aeb87492eaa00b813b0d5d07c4824c640480d712
ncore-x <135090444+ncore-x@users.noreply.github.com>
2023-06-11 23:23:05 +1100

    add models in shop to admin django

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name='product_name')
+    code = models.CharField(max_length=255, verbose_name='product_code')
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    unit = models.CharField(max_length=255, blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
+    note = models.TextField(blank=True, null=True)
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    time = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(blank=True, null=True)
+
+
+class Order(models.Model):
+    STATUS_CART = '1_cart'
+    STATUS_WAITING_FOR_PAYMENT = '2_waiting_for_payment'
+    STATUS_PAID = '3_paid'
+    STATUS_CHOICES = [
+        (STATUS_CART, 'cart'),
+        (STATUS_WAITING_FOR_PAYMENT, 'waiting_for_payment'),
+        (STATUS_PAID, 'paid')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # items = models.ManyToManyField(OrderItem, related_name='orders')
+    status = models.CharField(max_length=32, choices=STATUS_CHOICES, default=STATUS_CART)
+    amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    payment = models.ForeignKey(Payment, on_delete=models.PROTECT, blank=True, null=True)
+    comment = models.TextField(blank=True, null=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    discount = models.DecimalField(max_digits=20, decimal_places=2, default=0)
